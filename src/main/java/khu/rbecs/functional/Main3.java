e302db050db5cb9ec4e7c5c86f3b6da7e5bb65ce
obutr <butrym@karazin.ua>
2025-09-29 10:09:59 +0300

    Add StreamAPI demos

---
+++ src/main/java/khu/rbecs/functional/Main3.java
@@ -0,0 +1,111 @@
+package khu.rbecs.functional;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main3 {
+    // StreamAPI
+    public static void main(String... args) {
+        // terminal:
+        // forEach(), findFirst/findAny
+        // toList
+        var list = Stream.of("Alice")
+                .collect(Collectors.toList());
+//                .toList();
+        System.out.println(list);
+        list.add("Bob");
+        System.out.println(list);
+    }
+
+    public static void main7(String... args) {
+        // distinct/sorted
+        var rnd = new Random();
+        rnd.ints(1, 7)
+//                .filter(i -> i > 0)
+                .distinct()
+                .limit(5)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public static void main6(String... args) {
+        // filter/map
+        IntStream.rangeClosed(1, 4)
+                .flatMap(i -> {
+                    if (i % 3 == 2) {
+                        return null;
+                    } else {
+                        return IntStream.rangeClosed(1, i);
+                    }
+                })
+                .forEach(System.out::println);
+    }
+
+
+    public static void main5(String... args) {
+        IntStream.range(0, 5).forEach(i ->
+                System.out.println(i + "^2 = " + (i * i)));
+        IntStream.rangeClosed(0, 5).forEach(i ->
+                System.out.println(i + "^2 = " + (i * i)));
+        System.out.println("***");
+        IntStream.rangeClosed(0, 10)
+                .mapToDouble(n -> 0.1 * n)
+                .forEach(System.out::println);
+        System.out.println("***");
+        //                .limit(11)
+        DoubleStream.iterate(0, d -> d <= 1.05, d -> d + 0.1)
+                .forEach(System.out::println);
+    }
+
+    public static void main4(String... args) {
+        Stream.iterate(1, n -> n * 2)
+                .takeWhile(n -> n < 1_000)
+                .forEach(System.out::println);
+        System.out.println("***");
+        Stream.iterate(1, n -> n < 1_000, n -> n * 3)
+                .forEach(System.out::println);
+    }
+
+    public static void main3(String... args) {
+        // sources
+        var rnd = new Random();
+        Stream.generate(() -> rnd.nextInt(100))
+                .limit(10)
+                .takeWhile(n -> n < 98)
+                .skip(5)
+                .dropWhile(n -> n < 30)
+                .findFirst()
+                .ifPresentOrElse(
+                        System.out::println,
+                        () -> System.out.println("No number found")
+                );
+    }
+
+
+    public static void main2(String... args) {
+        Stream.of("Alice", "Bob", "Charlie", "John")
+                .filter(name -> name.length() > 3)
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+    }
+
+    public static void main1(String... args) {
+        // sources
+        var names = List.of("Alice", "Bob", "Charlie");
+        Stream<String> stream1 = names.stream();
+        // intermediate operations
+        // filter/map
+        Stream<String> stream2 = stream1.map(String::toUpperCase);
+//        Stream<Integer> stream3 = stream2.map(String::length);
+        Stream<String> stream3 = stream2
+                .filter(name -> name.length() > 3);
+        // terminal operation:
+        // reduce
+        List<String> list = stream3.toList();
+        System.out.println(list);
+    }
+}
