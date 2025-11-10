5788cd26156001f56697ce52fcf862dac7855093
obutr <butrym@karazin.ua>
2025-11-10 09:55:40 +0200

    Add Class.forName example

--- src/main/java/khu/rbecs/reflection/Main1.java
+++ src/main/java/khu/rbecs/reflection/Main1.java
@@ -6,7 +6,8 @@ public class Main1 {
     public static void main(String... args) throws Exception {
 //        B b = new B();
         System.out.println("Start");
-        Class<?> clazz1 = B.class;
+//        Class<?> clazz1 = B.class;
+        Class<?> clazz1 = Class.forName("khu.rbecs.reflection.B");
         System.out.println("after B.class access");
         Class<?> clazz2 = Class.forName("khu.rbecs.reflection.B");
         System.out.println("after Class.forName");
@@ -16,8 +17,8 @@ public class Main1 {
         ctor1.setAccessible(true);
         ctor2.setAccessible(true);
 
-        B b1 = (B) ctor1.newInstance();
-        B b2 = (B) ctor2.newInstance("hi");
+        Object b1 = /*(B)*/ ctor1.newInstance();
+        Object b2 = /*(B)*/ ctor2.newInstance("hi");
 
         Method foo = clazz2.getDeclaredMethod("foo");
         foo.setAccessible(true);
@@ -27,6 +28,7 @@ public class Main1 {
     }
 }
 
+/*
 class B {
     static {
         System.out.println("static block of B");
@@ -42,3 +44,4 @@ class B {
         System.out.println("foo i = " + i);
     }
 }
+*/
